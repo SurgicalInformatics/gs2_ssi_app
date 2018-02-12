d654caf3c755de2c4c5d25e3bcc8818c879a8635
riinuots <riinu.ots@gmail.com>
2018-02-12 18:32:54 +0000

    added units to LOS and time2op

--- 0_prep.R
+++ 0_prep.R
@@ -1,10 +1,24 @@
 load('gs2_ssi.rda')
 
+shinydata$los.quantiles %<>% 
+  fct_recode("<1 day"   = "[0,1]",
+             "2-3 days" = "(1,3]",
+             "4-7 days" = "(3,7]",
+             ">7 days" = "(7,180]"
+             )
+
+shinydata$time2op.hours.factor %<>%
+  fct_recode("<6 h"    = "<6",
+             "6-11 h"  = "6-11",
+             "12-23 h" = "12-23",
+             "24-47 h" = "24-47",
+             "48+ h"   =  "48+")
+
 save(shinydata, file = 'shinydata.rda')
 
 names(allvars_grouped)[2] = "Patient characteristics"
 names(allvars_grouped)[3] = "Operative characteristics"
 
-
-
 save(allvars_grouped, file = 'allvars_grouped.rda')
+
+
