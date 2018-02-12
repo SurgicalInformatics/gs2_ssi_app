c5c1a02d3e4fb45a4f1f650173fe1aab181dc296
riinuots <riinuots@gmail.com>
2018-02-12 17:45:20 +0000

    changed the appearance of sliders - now globalsurg pink

--- ui.R
+++ ui.R
@@ -1,6 +1,7 @@
 # https://argonaut.is.ed.ac.uk/shiny/rots/gs2_ssi/
 library(shiny)
 library(shinythemes)
+library(shinyBS)
 
 load('allvars_grouped.rda')
 
@@ -17,6 +18,31 @@ palettes = list(
 )
 
 shinyUI(fluidPage(
+  # tags -----------------------
+  tags$head(
+    #Using ionRangeSlider's javascript options you can hide/show selector labels and min/max labels
+    HTML("
+    <script>
+    $(document).ready(function(){
+        $(\".js-range-slider\").ionRangeSlider({
+        hide_min_max: true,
+        hide_from_to: true
+        });
+
+    });
+
+    </script>
+    ")
+  ),
+  tags$style(type = "text/css", "
+        .irs-bar {background: #FF0099; border: white;}
+        .irs-bar-edge {background: #FF0099; border: #FF0099;display: block;}
+        .irs-grid-pol {display: none;}
+        .irs-slider {width: 25px; height: 25px;}
+    #    .panel-heading {background-color: #ff8cd1!important}
+  "),
+  
+  # -------------
   theme = shinytheme("yeti"),
   #shinythemes::themeSelector(),
   titlePanel("GlobalSurg 2 dataset explorer"),
@@ -25,106 +51,111 @@ shinyUI(fluidPage(
     
     column(4, # A - input panel
            wellPanel(style = "background-color: #ffffff;", # begin_A1
-             h4('Main input parameters:'),
-             # A1.1 - explanatory, split, outcome
-             selectInput("explanatory1",
-                         label    = "Explanatory variable:",
-                         selected = c("hdi_tertile"),
-                         choices  = allvars_grouped,
-                         multiple = FALSE),
-             selectInput("explanatory2",
-                         label    = "Split by:",
-                         selected = c("ALL"),
-                         choices  = allvars_grouped,
-                         multiple = FALSE),
-             selectInput("outcome",
-                         label    = "Outcome variable:",
-                         selected = c("ssi_yn.collapsed"),
-                         choices  = allvars_grouped_outcome,
-                         multiple = FALSE),
-             checkboxInput("axis_relative",
-                           label = "Relative to total (x-axis to %)",
-                           value = FALSE)
+                     h4('Main input parameters:'),
+                     # A1.1 - explanatory, split, outcome --------------
+                     selectInput("explanatory1",
+                                 label    = "Explanatory variable:",
+                                 selected = c("hdi_tertile"),
+                                 choices  = allvars_grouped,
+                                 multiple = FALSE),
+                     selectInput("explanatory2",
+                                 label    = "Split by:",
+                                 selected = c("ALL"),
+                                 choices  = allvars_grouped,
+                                 multiple = FALSE),
+                     selectInput("outcome",
+                                 label    = "Outcome variable:",
+                                 selected = c("ssi_yn.collapsed"),
+                                 choices  = allvars_grouped_outcome,
+                                 multiple = FALSE),
+                     checkboxInput("axis_relative",
+                                   label = "Relative to total (x-axis to %)",
+                                   value = FALSE)
            ),
-           wellPanel(
-             h4('Advanced parameters:'),
-             fluidRow(
-               column(3,
-                      h5('Reverse order:')),
-               column(3,
-                      checkboxInput("rev_expl1",   "Explanat.",   TRUE) ),
-               column(2,
-                      checkboxInput("rev_expl2",   "Split",   FALSE) ),
-               column(4,
-                      checkboxInput("rev_outcome", "Outcome", FALSE) )
-             ),
-             fluidRow( # A1.4 - shift outcome levels
-               # the percentage label is only plotted for the first factor level (otherwise would start overlapping)
-               # this "shifter" is useful if you want another level to be the first one on the barplot
-               # this complements the Reverse order options.
-               sliderInput("fct_shift", "Shift outcome levels:",
-                           min = 0, max = 6, value = 0, step=1,
-                           ticks=TRUE),
-               fluidRow(
-                 column(3,
-                        h5('Remove from outcome:')),
-                 column(3,
-                        checkboxInput("rem_unkwn",   "Unknown",   FALSE) ),
-                 column(3,
-                        checkboxInput("rem_mis",   "Missing",   TRUE) )
-               )
-             )
-           ), #end_A1
-           wellPanel( # begin_A2
-             sliderInput("width",  "Plot Width (%)", min = 20, max = 100, value = 80, step=10),
-             sliderInput("height", "Plot Height (px)", min = 200, max = 1000, value = 400, step=50)
-           ), # end_A2
-           # begin_A3
-           column(6, #begin_A3.1
-                  wellPanel(
-                    checkboxGroupInput("subset1",
-                                       label     = ("Subsetting: included countries"),
-                                       #note the spaces after the names (e.g. 'Ideal ')
-                                       #that's because in this syntax, Shiny expects names and values to be different
-                                       #which would be useful if you data for, e.g. 1,2,3,4,5 instead of names
-                                       #without the spaces you get:
-                                       #ERROR: 'selected' must be the values instead of names of 'choices' for the input 'subset1'
-                                       choices   = list('High HDI '       = 'High',
-                                                        'Middle HDI '     = 'Middle',
-                                                        'Low HDI '        = 'Low')
-                                       ,selected = c('High', 'Middle', 'Low'))
-                    
-                  )
-           ), #end_A3.1
-           column(6, # begin_A3.2
-                  wellPanel(
-                    selectInput("my_palette",
-                                label = "Colour palette:",
-                                selected = c("Paired"),
-                                choices  = palettes,
-                                multiple = FALSE),
-                    checkboxInput("reverse_colours", "Reverse colours", FALSE),
-                    radioButtons('legend_columns', 'Legend columns',
-                                 choices  = c(1:3),
-                                 selected = 2,
-                                 inline   = TRUE),
-                    checkboxInput("perc_label", "% label:", TRUE),
-                    radioButtons('black_white', label=NULL,
-                                 choices = list(
-                                   'Black' = 'black',
-                                   'White' = 'white'
-                                 ),
-                                 selected = 'white',
-                                 inline=TRUE)
-                  )
-                  
-           )  # end_A3.2
-
+           bsCollapsePanel(style = "default", h4('Advanced parameters:'), #advanced panel -----------
+                           wellPanel(style = "background-color: #ffffff;",
+                           fluidRow(
+                             column(4,
+                                    h5('Remove from outcome:')),
+                             column(4,
+                                    checkboxInput("rem_unkwn",   "Unknown",   FALSE) ),
+                             column(4,
+                                    checkboxInput("rem_mis",   "Missing",   TRUE) )
+                           ),
+                           fluidRow( # reverse outcome levels ----------
+                                     column(3,
+                                            h5('Reverse order:')),
+                                     column(3,
+                                            checkboxInput("rev_expl1",   "Explanat.",   TRUE) ),
+                                     column(2,
+                                            checkboxInput("rev_expl2",   "Split",   FALSE)    ),
+                                     column(4,
+                                            checkboxInput("rev_outcome", "Outcome", FALSE)    )
+                           ),
+                           
+                           fluidRow( # A1.4 - shift outcome levels --------------
+                                     # the percentage label is only plotted for the first factor level (otherwise would start overlapping)
+                                     # this "shifter" is useful if you want another level to be the first one on the barplot
+                                     # this complements the Reverse order options.
+                                     column(12,
+                                            sliderInput("fct_shift", "Shift outcome levels:",
+                                                        min = 0, max = 6, value = 0, step=1,
+                                                        ticks=TRUE)
+                                     )
+                           )
+                           ), # end first wellpanel ---------
+                           
+                           #), #end_A1 wellpanel ------------
+                           wellPanel(style = "background-color: #ffffff;", # begin_A2 plot width ----------
+                                     sliderInput("width",  "Plot Width (%)", min = 20, max = 100, value = 80, step=10),
+                                     sliderInput("height", "Plot Height (px)", min = 200, max = 1000, value = 400, step=50)
+                           ), # end_A2 ----------
+                           # begin_A3
+                           column(6, # begin_A3.1 begin HDI subsets ----------
+                                  wellPanel(style = "background-color: #ffffff;",
+                                            checkboxGroupInput("subset1",
+                                                               label     = ("Subsetting: included countries"),
+                                                               #note the spaces after the names (e.g. 'Ideal ')
+                                                               #that's because in this syntax, Shiny expects names and values to be different
+                                                               #which would be useful if you data for, e.g. 1,2,3,4,5 instead of names
+                                                               #without the spaces you get:
+                                                               #ERROR: 'selected' must be the values instead of names of 'choices' for the input 'subset1'
+                                                               choices   = list('High HDI '       = 'High',
+                                                                                'Middle HDI '     = 'Middle',
+                                                                                'Low HDI '        = 'Low')
+                                                               ,selected = c('High', 'Middle', 'Low'))
+                                            
+                                  )
+                           ), # end_A3.1
+                           column(6, # begin_A3.2 palettes ----------
+                                  wellPanel(style = "background-color: #ffffff;",
+                                            selectInput("my_palette",
+                                                        label = "Colour palette:",
+                                                        selected = c("Paired"),
+                                                        choices  = palettes,
+                                                        multiple = FALSE),
+                                            checkboxInput("reverse_colours", "Reverse colours", FALSE),
+                                            radioButtons('legend_columns', 'Legend columns',
+                                                         choices  = c(1:3),
+                                                         selected = 2,
+                                                         inline   = TRUE),
+                                            checkboxInput("perc_label", "% label:", TRUE),
+                                            radioButtons('black_white', label=NULL,
+                                                         choices = list(
+                                                           'Black' = 'black',
+                                                           'White' = 'white'
+                                                         ),
+                                                         selected = 'white',
+                                                         inline=TRUE)
+                                  )
+                                  
+                           )) #end_A1 wellpanel ------------  # end_A3.2 advanced end ----------
            
-    ), #end_A
+           
+    ), #end_A   inputs end ----------------
     
     
-    
+    # main panel -----------
     column(8, 
            tabsetPanel(type = "tabs", 
                        tabPanel("Data",
@@ -139,16 +170,17 @@ shinyUI(fluidPage(
                                 includeMarkdown("lay_summary.md")),
                        tabPanel("Abstract", 
                                 br(),
-                                p("Full publication will be available on 13-February 2018"),
+                                p("Full publication will be available on 14-February 2018"),
                                 includeMarkdown("abstract.md"))
            )
     )
     
   ), 
+  # bottom info ----------
   fluidRow(
     column(12,
-    p("App information and open-source code: ",
-    a("https://github.com/riinuots/gs2_ssi_app", href="https://github.com/riinuots/gs2_ssi_app"))
+           p("App information and open-source code: ",
+             a("https://github.com/riinuots/gs2_ssi_app", href="https://github.com/riinuots/gs2_ssi_app"))
     ))
-  
+  # shinyappUI and fluidpage end -----------  
 ))
